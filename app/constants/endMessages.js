2dfc0aba164520f562be949b768e7023e7371ad3
rehan-hansaja <rehanhansaja01@gmail.com>
2024-03-02 10:19:09 +0530

    Update endMessages.js

--- app/constants/endMessages.js
+++ app/constants/endMessages.js
@@ -4,15 +4,11 @@ export const endMessages = {
         'Congratulations on your stellar quiz performance! I guess being a walking encyclopedia does have its perks.',
         'Bravo! Your ability to make the rest of us feel intellectually inferior is truly remarkable.',
         'Impressive job on acing the quiz! I bet you have a secret hotline to the quiz gods.',
-        "Congratulations on your perfect score! I'll just be over here questioning the purpose of my existence.",
         ],
     good: [
-        'Almost acing the quiz is like almost winning the lottery, it still counts for nothing.',
         "You did well on the quiz, but don't worry, there's always room for improvement. Maybe aim for a perfect score next time?",
         "Congratulations on your nearly-perfect quiz score. It's almost like you tried, but not quite enough.",
         "You were so close to a perfect score, it's almost like you were trying to make the rest of us feel better about ourselves.",
-        "You didn't get a perfect score on the quiz, but at least you got a participation trophy... oh wait, this isn't little league?",
-        "Your quiz performance was almost perfect, but unfortunately, 'almost' doesn't quite cut it in the real world.",
     ],
     bad: [
         'Impressive display of quiz mediocrity. Keep aiming for the stars!',
